a55277e1fd02597230a75a1e5282d7115ea0e0b4
EthanUphoff <ethan.uphoff@gmail.com>
2018-11-08 10:54:58 -0600

    Added 4 threads in TMPD
    TMPD is threaded, though only returns 0s.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,126 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+    private long globalResult = Integer.MAX_VALUE;
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+        Thread[] threads = new Thread[4];
+        for(int i = 0; i < 4; i++){
+            switch(i){
+                case 0 :
+                    threads[i] = new Thread(new LowerLeft(values));
+                    break;
+                case 1 :
+                    threads[i] = new Thread(new LowerRight(values));
+                    break;
+                case 2 :
+                    threads[i] = new Thread(new Center(values));
+                    break;
+                case 3 :
+                    threads[i] = new Thread(new UpperRight(values));
+                    break;
+            }
+            threads[i].start();
+        }
+        for(int i = 0; i < 4; i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return this.globalResult;
+        //throw new UnsupportedOperationException();
+    }
+
+    public void updateGlobalResult(long localResult){
+        if(this.globalResult > localResult){
+            this.globalResult = localResult;
+        }
+    }
+
+    public class LowerLeft implements Runnable{
+        private int[] values;
+        public LowerLeft(int[] values){
+            this.values = values;
+        }
+        public void run(){
+            long result = Integer.MAX_VALUE;
+            for (int i = 0; i < values.length/2; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            updateGlobalResult(result);
+        }
+    }
+
+    public class LowerRight implements Runnable{
+        private int[] values;
+        public LowerRight(int[] values){
+            this.values = values;
+        }
+
+        public void run(){
+            long result = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = values.length/2; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            updateGlobalResult(result);
+        }
+    }
+
+    public class Center implements Runnable{
+        private int[] values;
+        public Center(int[] values){
+            this.values = values;
+        }
+
+        public void run(){
+            long result = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = values.length/2; j <= i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[j] - values[i]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            updateGlobalResult(result);
+        }
+    }
+
+    public class UpperRight implements Runnable{
+        private int[] values;
+        public UpperRight(int[] values){
+            this.values = values;
+        }
+
+        public void run(){
+            long result = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = values.length/2; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            updateGlobalResult(result);
+        }
     }
 
 }
